565f0881acc7ca7290d72e02913b3208d9fea9f3
Nilesh Kumar <137385597+nk9000@users.noreply.github.com>
2023-12-13 02:19:44 +0530

    Add files via upload

---
+++ ser.js
@@ -0,0 +1,54 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Replace with your MongoDB Atlas connection string
+const mongoUri = 'mongodb+srv://nk:gssZwNyabkmgFKaQ@cluster0.qunq6jz.mongodb.net/';
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const Product = mongoose.model('Product', {
+  productName: String,
+  oneLiner: String,
+  price: Number,
+  quantity: Number,
+  sectionName: String,
+  image: String,
+});
+
+// API to fetch products
+app.get('/api/products', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// API to insert a new product
+app.post('/api/products', async (req, res) => {
+  try {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.status(201).json(newProduct);
+  } catch (error) {
+    console.error('Error inserting product:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
